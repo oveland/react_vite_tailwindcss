1dafa15ae93e0b58b6f3b73bbc1e8e9eb5b57f6c
Oscar <oveland@iMac-OV.local>
2024-06-07 11:36:48 -0500

    :wrench: Adds prettier-plugin-tailwindcss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,11 @@ import './App.scss';
 
 function App() {
   return (
-    <div className='app'>
-      <div className='app-message__container'>
-        <span className='app-message__title'>Bienvenido</span>
-        <span className='app-message__description' data-testid='description'>
+    <div className='flex h-screen items-center justify-center bg-gradient-to-br from-green-600 to-indigo-500'>
+      <div className='flex flex-col gap-4 text-center text-indigo-100'>
+        <span className='text-9xl font-semibold'>Bienvenido</span>
+
+        <span className='animate-pulse text-2xl' data-testid='description'>
           Vite + React + Tailwindcss + TS
         </span>
       </div>
